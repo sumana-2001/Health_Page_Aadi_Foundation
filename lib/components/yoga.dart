b631b6d911ef343ed9246e5a7bce61bcfae63f2a
sumana-2001 <81135533+sumana-2001@users.noreply.github.com>
2023-07-31 16:05:28 +0530

    Updated yoga.dart

--- lib/components/yoga.dart
+++ lib/components/yoga.dart
@@ -11,15 +11,16 @@ class yoga extends StatefulWidget {
   State<yoga> createState() => _yogaState();
 }
 
+List<String> continue_flows = ["assets/images/back_bending_pose.jpg","assets/images/cardio_yoga1.jpg","assets/images/lower_body_streches.png","assets/images/pigeon_pose.jpg","assets/images/streching_exercise.png","assets/images/yoga.png","assets/images/bridge_pose.jpg"];
 List<String> weekdays = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
-Map<String,List<String>> Tasks = {"Yoga":["assets/images/yoga.png"],"meditation":["assets/images/meditate.png"],"Stretches":["assets/images/streching.png"],"Cables":["assets/images/cables.jpeg"],"Band":["assets/images/bodyweight.png"],"Kettlebells":["assets/images/kettlebells.jpeg"],"BodyPartWise":["assets/images/man_front.png"]};
+Map<String,List<String>> Tasks = {"Yoga":["assets/images/yoga_3d.png"],"Meditation":["assets/images/meditation_3d.png"],"Stretches":["assets/images/streching_3d.png"],"Cables":["assets/images/cables_3d.jpeg"],"Bodyweight":["assets/images/body_lifting_3d.jpeg"],"Kettlebells":["assets/images/kettlebells_3d.jpeg"],"Male Anatomy":["assets/icons/man_front.png"],"Female Anatomy":["assets/icons/women_front.png"]};
 class _yogaState extends State<yoga> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         extendBody: true,
-        bottomNavigationBar: gbar(),
+        //bottomNavigationBar: gbar(),
         backgroundColor: Colors.white,
         body: CustomScrollView(
           physics: BouncingScrollPhysics(),
@@ -108,7 +109,7 @@ class _yogaState extends State<yoga> {
                                   alignment: Alignment.center,
                                   children: [
                                     Positioned(
-                                        child: Text("TRX Workouts"),
+                                        child: Text("Band Workouts"),
                                       top: 30,
                                       left:30,
                                     ),
@@ -120,13 +121,8 @@ class _yogaState extends State<yoga> {
                                             onPressed: (){
                                               Navigator.push(
                                                 context,
-                                                MaterialPageRoute(
-                                                    builder: (_) =>
-                                                        Fetch(
-                                                            choice: 2,
-                                                            name: "TRX"
-                                                        )
-                                                ),);
+                                                MaterialPageRoute(builder: (context) => Fetch(name: "Band", choice: 2, gender: "Male")),
+                                              );
                                             },
                                           child: Icon(Icons.play_arrow),
                                         )
@@ -149,7 +145,7 @@ class _yogaState extends State<yoga> {
                             children: [
                               Padding(
                                 padding: EdgeInsets.all(
-                                    20.0), // Adjust the padding value as per your requirement
+                                    30.0), // Adjust the padding value as per your requirement
                                 child: Text(
                                   'Recommended Flows',
                                   style: TextStyle(
@@ -160,14 +156,16 @@ class _yogaState extends State<yoga> {
                             ],
                           ),
                           Container(
-                            height: 200,
+                            height: MediaQuery.of(context).size.height - 480,
+
                             child:ListView.builder(
                                 itemCount: Tasks.length,
                                 scrollDirection: Axis.horizontal,
                                 physics: BouncingScrollPhysics(),
                                 shrinkWrap: true,
                                 itemBuilder:(context,index){
-                                  return Card(
+                                  return Column(
+                                      children : [Card(
                                     color: Colors.white,
                                     shape: RoundedRectangleBorder(
                                       side:BorderSide(
@@ -182,16 +180,13 @@ class _yogaState extends State<yoga> {
 
                                           child: Ink.image(
                                               image: AssetImage(Tasks.values.elementAt(index)[0]),
-                                              fit: BoxFit.cover,
-                                            height: 140,
-                                            width: 150,
+                                              //fit: BoxFit.cover,
+                                               fit: BoxFit.fill,
+                                            height: 200,
+                                            width: 200,
                                           ),
                                         ),
-                                        Positioned(
-                                           left : 3,
-                                            bottom : 4,
-                                            child: Text(Tasks.keys.elementAt(index))
-                                        ),
+
                                         Positioned(
                                           right: 10,
                                           bottom: 10,
@@ -201,38 +196,45 @@ class _yogaState extends State<yoga> {
                                               child: FloatingActionButton.small(
                                                 heroTag: index,
                                                   onPressed: (){
-                                                    if(Tasks.keys.elementAt(index) == "BodyPartWise"){
-                                                      Navigator.push(
-                                                        context,
-                                                        MaterialPageRoute(builder: (context) => musclewiki()),
-                                                      );
-                                                    }
-                                                    else {
-                                                      Navigator.push(
-                                                        context,
-                                                        MaterialPageRoute(
-                                                            builder: (_) =>
-                                                                Fetch(
-                                                                    choice: 2,
-                                                                    name: Tasks
-                                                                        .keys
-                                                                        .elementAt(
-                                                                        index)
-                                                                )
-                                                        ),);
-                                                    }
+                                                     if(Tasks.keys.elementAt(index) == "Male Anatomy"){
+                                                       Navigator.push(
+                                                         context,
+                                                         MaterialPageRoute(builder: (context) => musclewiki( gender: "Male")),
+                                                       );
+                                                     }
+                                                     else if(Tasks.keys.elementAt(index) == "Female Anatomy"){
+                                                       Navigator.push(
+                                                         context,
+                                                         MaterialPageRoute(builder: (context) => musclewiki(gender: "Female")),
+                                                       );
+                                                     }
+                                                     else if(Tasks.keys.elementAt(index) == "Meditation"){
+                                                       Navigator.push(
+                                                         context,
+                                                         MaterialPageRoute(builder: (context) => Fetch(name: "TRX", choice: 2, gender: "Male")),
+                                                       );
+                                                     }
+                                                     else{
+                                                       print(Tasks.keys.elementAt(index));
+                                                       Navigator.push(
+                                                         context,
+                                                         MaterialPageRoute(builder: (context) => Fetch(name: Tasks.keys.elementAt(index), choice: 2, gender: "Male")),
+                                                       );
+                                                     }
                                                   },
                                                 child: Icon(Icons.play_arrow,size: 20,),
                                                 backgroundColor: Colors.lightBlueAccent,
                                                   
                                               
                                               )
-                                            ))
+                                            )),
                                       ],
                                     ),
 
 
-                                  );
+                                  ),
+                                  Text(Tasks.keys.elementAt(index),style: TextStyle(fontWeight: FontWeight.bold,color:Colors.cyan,fontSize: 15),),
+                                  ]);
                                 } ),
                           ),
                           Row(
@@ -253,10 +255,10 @@ class _yogaState extends State<yoga> {
                             width: MediaQuery.of(context).size.width,
                             child: ListView.builder(
                               shrinkWrap: true,
-                              itemCount: 7,
+                              itemCount: continue_flows.length,
                               itemBuilder: (context, index) {
                                 // Generate a random image for demonstration purposes
-                                final randomImageUrl = 'https://media.emailonacid.com/wp-content/uploads/2019/03/2019-GifsInEmail.gif';
+                                final randomImage = continue_flows[index];
 
                                 return InkWell(
                                   onTap: () {
@@ -266,7 +268,7 @@ class _yogaState extends State<yoga> {
                                     );
                                   },
                                   child: ListTile(
-                                    leading: Image.network(randomImageUrl), // Random image
+                                    leading: Image.asset(randomImage), // Random image
                                     title: Text(
                                       weekdays[index],
                                       style: TextStyle(fontWeight: FontWeight.bold),
@@ -295,67 +297,67 @@ class _yogaState extends State<yoga> {
 }
 
 
-class gbar extends StatefulWidget {
-  const gbar({super.key});
-
-  @override
-  State<gbar> createState() => _gbarState();
-}
-
-class _gbarState extends State<gbar> {
-  @override
-  Widget build(BuildContext context) {
-    int _select=0;
-    double screenWidth = MediaQuery.of(context).size.width;
-    return Material(
-      color: Colors.transparent,
-      child:
-      ClipPath(
-        clipper: ProsteBezierCurve(
-          position: ClipPosition.top,
-          list: [
-            BezierCurveSection(
-              start: Offset(screenWidth, 20),
-              top: Offset(screenWidth / 2, 0),
-              end: Offset(0, 20),
-            ),
-          ],
-        ),
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 10),
-          color: Colors.white,
-          height: 110,
-          child: GNav(
-            gap: 8,
-            haptic: true,
-            color: Colors.grey,
-            activeColor: Colors.black,
-            tabBackgroundColor: Colors.lightBlueAccent.shade100,
-            padding: EdgeInsets.all(15),
-            tabs: [
-              GButton(
-                icon: Icons.home_outlined,
-                text: "Home",
-              ),
-              GButton(
-                icon: Icons.stacked_bar_chart_outlined,
-                text: "Classroom",
-              ),
-              GButton(
-                icon: Icons.shopping_bag_outlined,
-                text: "Meditate",
-              ),
-              GButton(
-                icon: Icons.person,
-                text: "Profile",
-              ),
-            ],
-            onTabChange: (index){
-              _select=index;
-            },
-          ),
-        ),
-      ),
-    );
-  }
-}
+// class gbar extends StatefulWidget {
+//   const gbar({super.key});
+//
+//   @override
+//   State<gbar> createState() => _gbarState();
+// }
+//
+// class _gbarState extends State<gbar> {
+//   @override
+//   Widget build(BuildContext context) {
+//     int _select=0;
+//     double screenWidth = MediaQuery.of(context).size.width;
+//     return Material(
+//       color: Colors.transparent,
+//       child:
+//       ClipPath(
+//         clipper: ProsteBezierCurve(
+//           position: ClipPosition.top,
+//           list: [
+//             BezierCurveSection(
+//               start: Offset(screenWidth, 20),
+//               top: Offset(screenWidth / 2, 0),
+//               end: Offset(0, 20),
+//             ),
+//           ],
+//         ),
+//         child: Container(
+//           padding: EdgeInsets.symmetric(horizontal: 10),
+//           color: Colors.white,
+//           height: 110,
+//           child: GNav(
+//             gap: 8,
+//             haptic: true,
+//             color: Colors.grey,
+//             activeColor: Colors.black,
+//             tabBackgroundColor: Colors.lightBlueAccent.shade100,
+//             padding: EdgeInsets.all(15),
+//             tabs: [
+//               GButton(
+//                 icon: Icons.home_outlined,
+//                 text: "Home",
+//               ),
+//               GButton(
+//                 icon: Icons.stacked_bar_chart_outlined,
+//                 text: "Stats",
+//               ),
+//               GButton(
+//                 icon: Icons.shopping_bag_outlined,
+//                 text: "Shop",
+//               ),
+//               GButton(
+//                 icon: Icons.person,
+//                 text: "Profile",
+//               ),
+//             ],
+//             onTabChange: (index){
+//               _select=index;
+//             },
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
